5ff56d6e9025efac877cbc62d707b1969060233b
David Bijl <davidbijl321@gmail.com>
2025-11-03 17:27:21 +0100

    connected api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,21 @@
 <script>
-    let trains = [];
+	import { onMount } from 'svelte';
+	let trains = [];
 
-    async function loadTrains() {
-        const response = await fetch('/api/trains');
-        trains = await response.json();
-    }
+	async function loadTrains() {
+		const response = await fetch('/api/trains');
+		const data = await response.json();
 
-    loadTrains();
+		console.log(data);
+
+		trains = data;
+	}
+
+	onMount(() => {
+		loadTrains();
+	});
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<style>
-
-</style>
+<div>
+    <h1>hallo</h1>
+</div>
